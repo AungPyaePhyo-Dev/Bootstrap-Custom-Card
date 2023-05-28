1b5ec62987427991ce8f8b0b7ce9ac07c00dcba8
AungPyaePhyo81 <aungpyaephyo@myansis.com>
2023-05-28 21:04:11 +0630

    Add Counter

---
+++ script.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    $('.count').waypoint(function(direction){  
+    $('.count').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+        countNum: countTo
+    },
+
+    {
+
+        duration: 4000,
+        easing:'linear',
+        step: function() {
+        $this.text(Math.floor(this.countNum));
+        },
+        complete: function() {
+        $this.text(this.countNum);
+        }
+    });  
+    
+    
+    });
+
+    },{
+        offset: '90%'
+});
+
+});
